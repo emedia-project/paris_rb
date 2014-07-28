acc1c626ae1abb33e3609f3b4d598ea10a687ab6
glejeune <gregoire.lejeune@free.fr>
2014-07-28 20:03:58 +0200

    Use new renderer

--- example/plugin_sample/apps/plugin_sample/src/controller/index.rb
+++ example/plugin_sample/apps/plugin_sample/src/controller/index.rb
@@ -1,26 +1,27 @@
 require 'paris' 
 require 'sample' ## This come from priv/ruby
+extend ErlPort::ErlTerm
 
 def get(_Request)
   if Sample.new.ok? 
-    ParisResponse.render_view(:ok)
+    ParisResponse.render(:html, [Tuple.new([:template, :ok])])
   else
-    ParisResponse.render_view(:index)
+    ParisResponse.render(:html, [Tuple.new([:template, :index])])
   end
 end
 
 def post(_Request)
-  ParisResponse.render_view(:index)
+  ParisResponse.render(:html, [Tuple.new([:template, :index])])
 end
 
 def put(_Request)
-  ParisResponse.render_view(:index)
+  ParisResponse.render(:html, [Tuple.new([:template, :index])])
 end
 
 def head(_Request)
-  ParisResponse.render_view(:index)
+  ParisResponse.render(:html, [Tuple.new([:template, :index])])
 end
 
 def delete(_Request)
-  ParisResponse.render_view(:index)
+  ParisResponse.render(:html, [Tuple.new([:template, :index])])
 end
